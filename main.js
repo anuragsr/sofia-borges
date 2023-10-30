26cee7b0021bbcff582217a443d1fba99b36dd02
Anurag <anurag.131092@gmail.com>
2023-10-30 12:29:18 +0530

    Added tailwind

--- main.js
+++ main.js
@@ -1,23 +1,12 @@
 import './style.sass'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Sofia!</h1>
+    <h1 class="text-7xl">Hello Sofia!</h1>
     <div class="card">
-      <button id="counter" type="button"></button>
+      <button id="counter" class="bg-green-700 p-8 text-3xl rounded-2xl shadow-2xl" type="button"></button>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
   </div>
 `
 
