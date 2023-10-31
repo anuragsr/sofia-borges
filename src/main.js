da196fb0681c8a5eaa4cea944a1f70567bd704fe
Anurag <anurag.131092@gmail.com>
2023-10-31 10:29:22 +0530

    Structure change

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <h1 class="text-7xl">Hello Sofia!</h1>
+    <h1 class="text-7xl">Hello Sofia 21!</h1>
     <div class="card">
-      <button id="counter" class="bg-green-700 p-8 text-3xl rounded-2xl shadow-2xl" type="button"></button>
+      <button id="counter" class="bg-green-700 p-8 text-3xl text-white rounded-2xl shadow-2xl" type="button"></button>
     </div>
   </div>
 `
