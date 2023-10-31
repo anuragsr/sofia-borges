058b6c6a5a4af0e53ebe13ae8341023da0b973b7
Anurag <anurag.131092@gmail.com>
2023-10-31 12:05:18 +0530

    Added partials, restructuring

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // import javascriptLogo from './javascript.svg' // same dir
 // import viteLogo from '/vite.svg' // Public folder
-import './style.sass'
-import { setupCounter } from './counter.js'
+import "./sass/style.sass";
+import { setupCounter } from "./counter.js";
 
-setupCounter(document.querySelector('#counter'))
+setupCounter(document.querySelector("#counter"));
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
-import { defineConfig } from "vite"
-import pugPlugin from "vite-plugin-pug"
+import { defineConfig } from "vite";
+import pugPlugin from "vite-plugin-pug";
+import { resolve } from "path";
 
-const options = { pretty: true } // FIXME: pug pretty is deprecated!
-const locals = { name: "My Pug" }
+const options = { pretty: true }; // FIXME: pug pretty is deprecated!
+const locals = { name: "My Pug" };
 
 export default defineConfig({
-	plugins: [pugPlugin(options, locals)],
-})
+  plugins: [pugPlugin(options, locals)],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about.html"),
+      },
+    },
+  },
+});
