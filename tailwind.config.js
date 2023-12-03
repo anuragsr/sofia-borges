e2f89d2a1d0a1b9be4f26af9b97c9410dc1b11c9
Anurag <anurag.131092@gmail.com>
2023-12-03 15:14:27 +0530

    Mobile | Hero images WIP

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,11 @@
 export default {
   content: ["./src/**/*.{html,pug,js}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        9: "repeat(9, minmax(0, 1fr))",
+      },
+    },
   },
   plugins: [],
-}
-
+};
